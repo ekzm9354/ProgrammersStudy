a54238fde75386a80ce744237675afd2d9af2702
Su Min Kim <106305465+ekzm9354@users.noreply.github.com>
2023-03-07 21:44:01 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer={};
+        int[] temp = new int[numlist.length];
+        int cnt = 0;
+        for(int i=0; i<numlist.length;i++){
+            if(numlist[i]%n==0){
+             temp[cnt]=numlist[i];
+                cnt++;
+            }
+        }
+        answer = new int[cnt];
+        for(int i=0;i<cnt;i++){
+          answer[i]=temp[i];
+        }
+        return answer;
+    }
+}
